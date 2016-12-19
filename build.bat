3e89540692397f1bf6ddf609c0c54a27be760fbe
Zousiyu <zousiyu1995@163.com>
2016-12-19 11:00:48 +0800

    继续完善列表环境

--- build.bat
+++ build.bat
@@ -1,19 +1,14 @@
-:: Copyright (c) 2012-2016 hzz
+:: Copyright (c) 2012-2016 Zousiyu
 
 @echo off
-
 :: compile the tex file
-
 xelatex.exe --synctex=-1 main.tex
 
 ::pause
-
 biber main
 
 ::pause
-
 xelatex.exe --synctex=-1 main.tex
 
 :: clear aux files
-
 call clear
