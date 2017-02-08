7b525db84109da315634d9d025a29c8c3ee3659e
Zousiyu <zousiyu1995@outlook.com>
2017-02-08 21:18:24 +0800

    添加索引

--- clear.bat
+++ clear.bat
@@ -1,2 +1,2 @@
 @echo off
-del /q *.aux *.bbl *.bcf *.blg *.listing *.log *.out *.xml *.toc *.lof *.lot
+del /q *.aux *.bbl *.bcf *.blg *.listing *.log *.out *.xml *.toc *.lof *.lot *.idx *.ilg *.ind
